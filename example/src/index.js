b1d27c3af182de038c8135f6c02993a0f0a4e318
guoshencheng <648772021@qq.com>
2017-03-19 16:01:30 +0800

    add example for test drive develop

--- example/src/index.js
+++ example/src/index.js
@@ -2,8 +2,21 @@
  * Created by guoshencheng on 18/03/2017.
  */
 
-import { Component, CreateElement } from 'peact';
-import itemContainer from './itemContainer';
+var PIXI = require('pixi');
+
+import { Component, CreateElement, Render } from '../../lib';
+
+class CustomItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var props = {
+      x: 12, y: 12
+    }
+    return CreateElement('Sprite', props, null)
+  }
+}
 
 class CustomContainer extends Component {
   constructor(props) {
@@ -12,8 +25,22 @@ class CustomContainer extends Component {
   render() {
     var props = { x: 12, y: 12 };
     return CreateElement('Container', props, [
-      CreateElement(itemContainer, null, null),
-      CreateElement('Text', null, null)
+      CreateElement(CustomItem, null, null),
+      CreateElement("Container", null, [
+        CreateElement(CustomItem, null, null),
+        CreateElement(CustomItem, null, null)
+      ])
     ]);
   }
 }
+
+console.log(new CustomContainer().render());
+
+var canvas = document.querySelector("#canvas");
+var app = new PIXI.Application(640, 1136, {
+  backgroundColor : 0x1099bb,
+  view: canvas
+});
+
+Render(CreateElement(CustomContainer, null, null), app);
+console.log(app)
---
+++ webpack.config.example.js
@@ -0,0 +1,33 @@
+var webpack = require('webpack')
+var path = require('path')
+var fs = require('fs')
+var appDir = path.resolve(__dirname, './src/apps')
+
+module.exports = {
+  devtool: "source-map",
+  entry: {
+    example: './example/src/index.js'
+  },
+  output: {
+      path: __dirname + "/example/dist/",
+      filename: "[name].js",
+      publicPath: "/example/dist/"
+  },
+  externals:{
+    react:'React',
+    'react-dom':'ReactDOM',
+    pixi: 'PIXI'
+  },
+  module: {
+    loaders: [
+      {
+        test: /.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+}
