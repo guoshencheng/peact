5ceee23b05f57c6659932a3f36016dcb3414ba54
guoshencheng <648772021@qq.com>
2017-03-26 13:05:13 +0800

    add PixiNativeBuilder for build native pixi element and add example which rendered sprite and animatedSprite whith lib

--- example/src/index.js
+++ example/src/index.js
@@ -11,10 +11,32 @@ class CustomItem extends Component {
     super(props);
   }
   render() {
-    var props = {
-      x: 12, y: 12
-    }
+    var textures = PIXI.Texture.fromImage('/example/assets/logo.png');
+    var props = Object.assign({
+      "scale.x": 0.3,
+      "scale.y": 0.3,
+      textures,
+    }, this.props);
     return CreateElement('Sprite', props, null)
+  }
+}
+
+class Fighter extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    var textures = [];
+    for (var i = 0; i < 30; i++) {
+      var val = i < 10 ? '0' + i : i;
+      textures.push(PIXI.Texture.fromFrame('rollSequence00' + val + '.png'));
+    }
+    var props = Object.assign({
+      x:200,
+      y: 200,
+      textures,
+    }, this.props);
+    return CreateElement('AnimatedSprite', props, null)
   }
 }
 
@@ -22,19 +44,26 @@ class CustomContainer extends Component {
   constructor(props) {
     super(props);
   }
+
+  buildItems() {
+    var a = [0, 1, 2, 3, 4];
+    return a.map(value => {
+      var props = {
+        x: 20,
+        y: value * 150
+      }
+      return CreateElement(CustomItem, props, null);
+    })
+  }
+
   render() {
     var props = { x: 12, y: 12 };
     return CreateElement('Container', props, [
-      CreateElement(CustomItem, null, null),
-      CreateElement("Container", null, [
-        CreateElement(CustomItem, null, null),
-        CreateElement(CustomItem, null, null)
-      ])
+      CreateElement("Container", null, this.buildItems()),
+      CreateElement(Fighter)
     ]);
   }
 }
-
-console.log(new CustomContainer().render());
 
 var canvas = document.querySelector("#canvas");
 var app = new PIXI.Application(640, 1136, {
@@ -42,5 +71,8 @@ var app = new PIXI.Application(640, 1136, {
   view: canvas
 });
 
-Render(CreateElement(CustomContainer, null, null), app);
+
+PIXI.loader.add('/example/assets/logo.png').add('/example/assets/fighter.json').load(() => {
+  Render(CreateElement(CustomContainer, null, null), app);
+});
 console.log(app)
